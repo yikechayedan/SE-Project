42e5a04e87b49abec7436867640542eb753abd49
wang-ty22 <wang-ty22@mails.tsinghua.edu.cn>
2025-12-03 20:10:08 +0800

    feat: 完善数据集上传下载功能

--- PolyMetric/backend/apps/datasets/urls.py
+++ PolyMetric/backend/apps/datasets/urls.py
@@ -3,9 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import DatasetViewSet
 
 router = DefaultRouter()
-router.register(r"datasets", DatasetViewSet)  # 数据集接口前缀：/api/datasets/
+router.register(r"datasets", DatasetViewSet, basename="dataset")
 
 urlpatterns = [
-    path("my_datasets/", DatasetViewSet.as_view({"get": "my_datasets"})),
     path("", include(router.urls)),
 ]
